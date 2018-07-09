359f00324510c8dba883fdce6bce99e200a2c5e6
AnastasiyaDemi <dem.nasti@gmail.com>
2018-07-10 03:51:47 +0400

    Point3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,24 +5,25 @@ public class Point {
     public double y;
     public double n;
 
-    public static void main(String [] args) {
-    Point p1 = new Point(2,3,1);
-    Point p2 = new Point(1,2,2);
-    distance(p1, p2);
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 3, 1);
+        Point p2 = new Point(1, 2, 2);
+        distance(p1, p2);
 
     }
 
-    public Point(double x, double y, int n ) {
-        this.x=x;
-        this.y=y;
-        this.n=n;
-        System.out.println("p" +n+ " это точка с координатами"+" "+ x + " и " + y);
+    public Point(double x, double y, int n) {
+        this.x = x;
+        this.y = y;
+        this.n = n;
+        System.out.println("p" + n + " - это точка с координатами: " + x + " " + y);
 
     }
 
-    public static double distance(Point p1, Point p2){
-        System.out.println ("Расстояние между двумя заданными точками р1 и р2 = " + Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y)));
-        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    public static double distance(Point p1, Point p2) {
+        double result = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+        System.out.println("Расстояние между двумя заданными точками р1 и р2 = " + result);
+        return result;
     }
 
 }
