6a67a0221d556c4db22b1bb187b652c44c9e9d90
AnastasiyaDemi <dem.nasti@gmail.com>
2018-07-18 14:39:33 +0400

    новая попытка по заданию 2 + создание пакета для теста

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,26 +3,23 @@ package ru.stqa.pft.sandbox;
 public class Point {
     public double x;
     public double y;
-    public double n;
 
     public static void main(String[] args) {
-        Point p1 = new Point(2, 3, 1);
-        Point p2 = new Point(1, 2, 2);
-        distance(p1, p2);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+        double d = p1.distance(p2);
+        System.out.println("Расстояние между двумя заданными точками р1 и р2 = " + d);
 
     }
 
-    public Point(double x, double y, int n) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
-        this.n = n;
-        System.out.println("p" + n + " - это точка с координатами: " + x + " " + y);
 
     }
 
-    public static double distance(Point p1, Point p2) {
-        double result = Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
-        System.out.println("Расстояние между двумя заданными точками р1 и р2 = " + result);
+    public double distance(Point p2) {
+        double result = Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
         return result;
     }
 
