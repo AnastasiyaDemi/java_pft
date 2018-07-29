c05b10f44bbdc1fa541edc525180621ac61668e6
AnastasiyaDemi <39959329+AnastasiyaDemi@users.noreply.github.com>
2018-07-29 14:58:40 +0200

    formatting

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,21 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String [] args) {
-		hello ("world");
-		hello ("Nik");
-		hello ("everybody");
 
-		Square s = new Square(5);
-		System.out.println ("Площадь квадрата со стороной "+s.l+" = "+s.area());
+    public static void main(String[] args) {
+        hello("world");
+        hello("Nik");
+        hello("everybody");
 
-		Rectangle r = new Rectangle(2,6);
-        System.out.println ("Площадь прямоугольника со сторонами "+r.a+ " и "+r.b+" = "+r.area());
-	}
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-	public static void hello (String somebody) {
-		System.out.println("Hello,"+ somebody + "!");
-	}
+        Rectangle r = new Rectangle(2, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
 
 }
