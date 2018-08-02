abee8d7b753b6353878e3ebd2a8c44ae830e0b64
AnastasiyaDemi <39959329+AnastasiyaDemi@users.noreply.github.com>
2018-08-02 14:55:02 +0200

    Задание 6: создание GroupModificationTests, ContactDeletionTests, ContactModificationTests,

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -39,4 +39,12 @@ public class ContactHelper extends HelperBase {
     public void deleteSelectedContact() {
         click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
     }
+
+    public void initContactModification() {
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+        click(By.name("update"));
+    }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,11 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getContactHelper().selectContact();
-
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new ContactData("2first name", "2last name", "2nik", "Address", "+79269998877", "+79031113366", "email@mail.ru"));
+        app.getContactHelper().submitContactModification();
+        app.getContactHelper().returnToHomePage();
     }
 }
