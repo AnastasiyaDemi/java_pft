f741ef93bfe2d68f61c0d7f97bff6005741019c6
Anastasiya <39959329+AnastasiyaDemi@users.noreply.github.com>
2018-12-10 22:42:54 +0100

    Задание №7: Научиться запускать тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,19 +1,34 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
 
-      public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(false));
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public void init() {
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         gotoHomePage();
         contactHelper = new ContactHelper(wd);
@@ -21,7 +36,7 @@ public class ApplicationManager {
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         sessionHelper.login("admin", "secret");
-        }
+    }
 
     public void gotoHomePage() {
         wd.get("http://addressbook.slashedzero.com:8082/");
@@ -40,6 +55,6 @@ public class ApplicationManager {
     }
 
     public ContactHelper getContactHelper() {
-         return contactHelper;
-      }
+        return contactHelper;
+    }
 }
