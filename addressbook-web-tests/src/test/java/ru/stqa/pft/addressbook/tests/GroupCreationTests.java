4eeb43001e3c7207c3b0761e8929ae479df6582c
Anastasiya <39959329+AnastasiyaDemi@users.noreply.github.com>
2018-12-25 21:00:20 +0100

    загружаем тестовые данные при создании групп из внешнего Json

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -25,8 +28,25 @@ public class GroupCreationTests extends TestBase {
     }
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/group.xml")));
+    public Iterator<Object[]> validGroupsAsJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+
+        return groups.stream()
+                .map((g) -> new Object[] {g})
+                .collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validGroupsAsXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
         String xml = "";
         String line = reader.readLine();
         while (line != null) {
@@ -36,13 +56,12 @@ public class GroupCreationTests extends TestBase {
         XStream xstream = new XStream();
         xstream.processAnnotations(GroupData.class);
         List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
-        List<Object[]> wrapped = groups.stream()
+        return groups.stream()
                 .map((g) -> new Object[] {g})
-                .collect(Collectors.toList());
-        return wrapped.iterator();
+                .collect(Collectors.toList()).iterator();
     }
 
-    @Test(dataProvider = "validGroups")
+    @Test(dataProvider = "validGroupsAsJson")
     public void testGroupCreation(GroupData group) {
         System.out.println(group.toString());
         Groups before = app.group().all();
