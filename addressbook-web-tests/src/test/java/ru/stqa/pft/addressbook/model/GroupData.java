68f6fd4b78780d2e8a79ff7eb6fdc22d311f2370
Anastasiya <39959329+AnastasiyaDemi@users.noreply.github.com>
2019-01-10 17:35:12 +0100

    работа с базами данных. Начало. Группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,19 +3,33 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias ("group")
+@Entity // объявляет класс GroupData привязанным к базе
+@Table (name = "group_list")
 public class GroupData {
 
     @XStreamOmitField
+    @Id
+    @Column (name = "group_id")
     private int id = 0;
     @Expose
+    @Column (name = "group_name")
     private String name;
     @Expose
+    @Column (name = "group_header")
+    @Type(type = "text")
     private String header;
     @Expose
+    @Column (name = "group_footer")
+    @Type(type = "text")
     private String footer;
 
     public int getId() {
